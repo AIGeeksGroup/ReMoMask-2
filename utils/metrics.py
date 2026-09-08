6daecbf5c14483baafc86c85fe010a754f02e777
Yiran Wang <63005381+u7079256@users.noreply.github.com>
2026-09-08 19:06:49 +0200

    Publish ReMoMask-2 utils

---
+++ utils/metrics.py
@@ -0,0 +1,87 @@
+import numpy as np
+from scipy import linalg
+import torch
+
+def calculate_mpjpe(gt_joints, pred_joints):
+    assert gt_joints.shape == pred_joints.shape, f'GT shape: {gt_joints.shape}, pred shape: {pred_joints.shape}'
+    pelvis = gt_joints[:, [0]].mean(1)
+    gt_joints = gt_joints - torch.unsqueeze(pelvis, dim=1)
+    pelvis = pred_joints[:, [0]].mean(1)
+    pred_joints = pred_joints - torch.unsqueeze(pelvis, dim=1)
+    mpjpe = torch.linalg.norm(pred_joints - gt_joints, dim=-1)
+    mpjpe_seq = mpjpe.mean(-1)
+    return mpjpe_seq
+
+def euclidean_distance_matrix(matrix1, matrix2):
+    assert matrix1.shape[1] == matrix2.shape[1]
+    d1 = -2 * np.dot(matrix1, matrix2.T)
+    d2 = np.sum(np.square(matrix1), axis=1, keepdims=True)
+    d3 = np.sum(np.square(matrix2), axis=1)
+    dists = np.sqrt(d1 + d2 + d3)
+    return dists
+
+def calculate_top_k(mat, top_k):
+    size = mat.shape[0]
+    gt_mat = np.expand_dims(np.arange(size), 1).repeat(size, 1)
+    bool_mat = mat == gt_mat
+    correct_vec = False
+    top_k_list = []
+    for i in range(top_k):
+        correct_vec = correct_vec | bool_mat[:, i]
+        top_k_list.append(correct_vec[:, None])
+    top_k_mat = np.concatenate(top_k_list, axis=1)
+    return top_k_mat
+
+def calculate_R_precision(embedding1, embedding2, top_k, sum_all=False):
+    dist_mat = euclidean_distance_matrix(embedding1, embedding2)
+    argmax = np.argsort(dist_mat, axis=1)
+    top_k_mat = calculate_top_k(argmax, top_k)
+    if sum_all:
+        return top_k_mat.sum(axis=0)
+    else:
+        return top_k_mat
+
+def calculate_activation_statistics(activations):
+    mu = np.mean(activations, axis=0)
+    cov = np.cov(activations, rowvar=False)
+    return (mu, cov)
+
+def calculate_diversity(activation, diversity_times):
+    assert len(activation.shape) == 2
+    assert activation.shape[0] > diversity_times
+    num_samples = activation.shape[0]
+    first_indices = np.random.choice(num_samples, diversity_times, replace=False)
+    second_indices = np.random.choice(num_samples, diversity_times, replace=False)
+    dist = linalg.norm(activation[first_indices] - activation[second_indices], axis=1)
+    return dist.mean()
+
+def calculate_multimodality(activation, multimodality_times):
+    assert len(activation.shape) == 3
+    assert activation.shape[1] > multimodality_times
+    num_per_sent = activation.shape[1]
+    first_dices = np.random.choice(num_per_sent, multimodality_times, replace=False)
+    second_dices = np.random.choice(num_per_sent, multimodality_times, replace=False)
+    dist = linalg.norm(activation[:, first_dices] - activation[:, second_dices], axis=2)
+    return dist.mean()
+
+def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-06):
+    mu1 = np.atleast_1d(mu1)
+    mu2 = np.atleast_1d(mu2)
+    sigma1 = np.atleast_2d(sigma1)
+    sigma2 = np.atleast_2d(sigma2)
+    assert mu1.shape == mu2.shape, 'Training and test mean vectors have different lengths'
+    assert sigma1.shape == sigma2.shape, 'Training and test covariances have different dimensions'
+    diff = mu1 - mu2
+    (covmean, _) = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
+    if not np.isfinite(covmean).all():
+        msg = 'fid calculation produces singular product; adding %s to diagonal of cov estimates' % eps
+        print(msg)
+        offset = np.eye(sigma1.shape[0]) * eps
+        covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
+    if np.iscomplexobj(covmean):
+        if not np.allclose(np.diagonal(covmean).imag, 0, atol=0.001):
+            m = np.max(np.abs(covmean.imag))
+            raise ValueError('Imaginary component {}'.format(m))
+        covmean = covmean.real
+    tr_covmean = np.trace(covmean)
+    return diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean
---
+++ utils/utils.py
@@ -0,0 +1,22 @@
+import math
+import time
+
+def print_current_loss(start_time, niter_state, total_niters, losses, epoch=None, sub_epoch=None, inner_iter=None, tf_ratio=None, sl_steps=None):
+
+    def as_minutes(s):
+        m = math.floor(s / 60)
+        s -= m * 60
+        return '%dm %ds' % (m, s)
+
+    def time_since(since, percent):
+        now = time.time()
+        s = now - since
+        es = s / percent
+        rs = es - s
+        return '%s (- %s)' % (as_minutes(s), as_minutes(rs))
+    if epoch is not None:
+        print('ep/it:%2d-%4d niter:%6d' % (epoch, inner_iter, niter_state), end=' ')
+    message = ' %s completed:%3d%%)' % (time_since(start_time, niter_state / total_niters), niter_state / total_niters * 100)
+    for (k, v) in losses.items():
+        message += ' %s: %.4f ' % (k, v)
+    print(message)
